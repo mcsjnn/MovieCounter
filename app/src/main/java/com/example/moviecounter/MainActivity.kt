145358666c0740a77c233726925e6384c4e742e4
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-09-19 21:23:29 -0500

    Add: Proyecto base

---
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.moviecounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviecounter.ui.theme.MovieCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieCounterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MovieCounter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    var movieName by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "You have added $count movies.")
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = movieName,
+            onValueChange = { movieName = it },
+            label = { Text("Movie Name") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            if (movieName.isNotBlank()) {
+                count++
+                movieName = ""
+            }
+        }) {
+            Text("Add Movie")
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMovieCounter() {
+    MovieCounter()
+}
